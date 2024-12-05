58059fbc98cf324076281242bad71e25c5d00a05
Gunn <155942588+gunnkrrish@users.noreply.github.com>
2024-12-05 16:36:34 +0530

    5 dec

---
+++ DSA/Function_01.java
@@ -0,0 +1,11 @@
+package DSA;
+
+public class Function_01 {
+    public static int add (int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int sum= add(5,3);
+        System.out.println(sum);
+    }
+}
---
+++ First_01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class First_01 {
+    public static void main(String[] args) {
+        System.out.println("enter a num:");
+        Scanner sc= new Scanner(System.in);
+        int num= sc.nextInt();
+        //System.out.println(num);
+        System.out.println(num);
+        }
+}
+
+
