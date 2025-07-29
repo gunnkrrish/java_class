6916038cd21056c700f03257503d1bccc1535682
Gunn <155942588+gunnkrrish@users.noreply.github.com>
2025-07-29 20:56:29 +0530

    29

---
+++ BEE/api/index.js
@@ -0,0 +1,44 @@
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// // const port = 8008;
+
+// // Tell Express where to find the views folder
+// app.set("views", path.join(__dirname, "../views"));
+// app.set("view engine", "ejs");
+
+// // Serve static files
+// app.use(express.static(path.join(__dirname, "../public")));
+
+// app.get("/", (req, res) => {
+//     res.render("home");
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+// module.exports = app;
+
+
+
+// //git koi empty folder ka track ni rkhta(and u want ki dikahye to .gitkeep ka use krna pdega) ni use extesnion auto gitkeep
+
+const express = require('express');
+const app= express();
+const path=require('path');
+
+const ejs=require('ejs');
+
+app.set('view engine','ejs')
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("views",path.join(__dirname,"views"));
+
+
+
+app.get('/',(req,res)=>{
+    
+    res.render("home");
+})
+
+module.exports = app;
